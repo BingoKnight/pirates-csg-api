514b1177abc9c95cb7d1255e470e43df2e9c6a0a
Nathan Hessling <nrhessling@gmail.com>
2023-01-10 22:21:22 -0500

    remove vulnerability that gives anyone the ability to modify models or keywords

--- routes/csgModel.js
+++ routes/csgModel.js
@@ -6,10 +6,10 @@ import { capitalize } from '../utils/string.js'
 
 const router = express.Router();
 
-router.put('/import', auth(), async (req, res, next) => {
-    console.log(req.body)
-    res.send(await importCsgModels(req.body))
-})
+// router.put('/import', auth(), async (req, res, next) => {
+//     console.log(req.body)
+//     res.send(await importCsgModels(req.body))
+// })
 
 router.get('', async (req, res, next) => {
     const piratesCsgArray = await queryCsgModels(req.query)
@@ -20,40 +20,40 @@ router.get('', async (req, res, next) => {
     res.send(resJson)
 })
 
-router.put('/keywords', auth(), async (req, res) => {
-    const piratesCsgArray = await queryCsgModels(req.query)
-    const updatedPiratesCsgArray = piratesCsgArray.map(csgItem => {
-        const newKeywords = req.body.keywords
-            .map(t => capitalize(t))
-            .filter(keyword => !csgItem.keywords.includes(keyword))
-        csgItem.keywords.push(...newKeywords)
-        return csgItem
-    })
-    const failures = await importCsgModels(updatedPiratesCsgArray)
-
-    res.send({
-        count: updatedPiratesCsgArray.length,
-        failures,
-        updatedModels: updatedPiratesCsgArray
-    })
-})
-
-router.delete('/keywords', auth(), async (req, res) => {
-    const {id, set, keyword} = req.query
-    const piratesCsgArray = await queryCsgModels({id, set})
-    const updatedPiratesCsgArray = piratesCsgArray.map(csgItem => {
-        const updatedKeywords = csgItem.keywords.filter(k => k !== keyword)
-        csgItem.keywords = [...new Set(updatedKeywords)]
-        return csgItem
-    })
-    const failures = await importCsgModels(updatedPiratesCsgArray)
-
-    res.send({
-        count: updatedPiratesCsgArray.length,
-        failures,
-        updatedModels: updatedPiratesCsgArray
-    })
-})
+// router.put('/keywords', auth(), async (req, res) => {
+//     const piratesCsgArray = await queryCsgModels(req.query)
+//     const updatedPiratesCsgArray = piratesCsgArray.map(csgItem => {
+//         const newKeywords = req.body.keywords
+//             .map(t => capitalize(t))
+//             .filter(keyword => !csgItem.keywords.includes(keyword))
+//         csgItem.keywords.push(...newKeywords)
+//         return csgItem
+//     })
+//     const failures = await importCsgModels(updatedPiratesCsgArray)
+//
+//     res.send({
+//         count: updatedPiratesCsgArray.length,
+//         failures,
+//         updatedModels: updatedPiratesCsgArray
+//     })
+// })
+//
+// router.delete('/keywords', auth(), async (req, res) => {
+//     const {id, set, keyword} = req.query
+//     const piratesCsgArray = await queryCsgModels({id, set})
+//     const updatedPiratesCsgArray = piratesCsgArray.map(csgItem => {
+//         const updatedKeywords = csgItem.keywords.filter(k => k !== keyword)
+//         csgItem.keywords = [...new Set(updatedKeywords)]
+//         return csgItem
+//     })
+//     const failures = await importCsgModels(updatedPiratesCsgArray)
+//
+//     res.send({
+//         count: updatedPiratesCsgArray.length,
+//         failures,
+//         updatedModels: updatedPiratesCsgArray
+//     })
+// })
 
 export default router
 
