96c2eb9b1463112d875b894d6d03d4590f30afa0
Nathan Hessling <nrhessling@gmail.com>
2023-05-23 19:01:22 -0400

    remove terraform

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-aws ecr get-login-password --region us-east-1 --profile pirates-csg-admin | docker login --username AWS --password-stdin 785117170351.dkr.ecr.us-east-1.amazonaws.com
+ECR_REPO=.dkr.ecr.us-east-1.amazonaws.com
+SERVICE_NAME=pirates-csg-api
+ECR_IMAGE=${SERVICE_NAME}:latest
+CONTAINER_PORT=8080
+HOST_PORT=8080
 
-docker build -t pirates-csg-api .
+aws ecr get-login-password --region us-east-1 --profile pirates-csg-admin | docker login --username AWS --password-stdin ${ECR_REPO}
 
-docker tag pirates-csg-api:latest 785117170351.dkr.ecr.us-east-1.amazonaws.com/pirates-csg-api:latest
+docker build -t ${SERVICE_NAME} .
 
-docker push 785117170351.dkr.ecr.us-east-1.amazonaws.com/pirates-csg-api:latest
+docker tag ${ECR_IMAGE} ${ECR_REPO}/${ECR_IMAGE}
+docker push ${ECR_REPO}/${ECR_IMAGE}
 
-aws ecs update-service --service pirates-csg-api --cluster pirates-csg-api-cluster --force-new-deployment --profile pirates-csg-admin
+ssh -T pirates-csg-server <<EOF
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REPO}
+docker stop ${SERVICE_NAME};
+docker rm ${SERVICE_NAME};
+docker pull ${ECR_REPO}/${ECR_IMAGE};
+docker run --name ${SERVICE_NAME} -p ${HOST_PORT}:${CONTAINER_PORT} --env-file .env ${ECR_REPO}/${ECR_IMAGE};
+EOF
 
--- terraform/acm.tf
+++
@@ -1,6 +0,0 @@
-data "aws_acm_certificate" "pirates_csg_certificate" {
-  domain      = "*.piratescsg.net"
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
-}
-
